53ad589539aca626767747116a7ddced3534809d
dvhungdev <68392174+dvhungdev@users.noreply.github.com>
2021-07-19 11:48:52 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,77 +1,9 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { boxesIntersect, useSelectionContainer } from "react-drag-to-select";
-
-const MouseSelection = React.memo(({ onSelectionChange }) => {
-  const { DragSelection } = useSelectionContainer({
-    eventsElement: document.getElementById("root"),
-    onSelectionChange,
-    onSelectionStart: () => {
-      console.log("OnSelectionStart");
-    },
-    onSelectionEnd: () => console.log("OnSelectionEnd")
-  });
-
-  return <DragSelection />;
-});
-
-const App = () => {
-  const [selectionBox, setSelectionBox] = useState();
-  const [selectedIndexes, setSelectedIndexes] = useState([]);
-  const selectableItems = useRef([]);
-
-  useEffect(() => {
-    const elementsContainer = document.getElementById("elements-container");
-    if (elementsContainer) {
-      Array.from(elementsContainer.childNodes).forEach((item) => {
-        const { left, top, width, height } = item.getBoundingClientRect();
-        selectableItems.current.push({
-          left,
-          top,
-          width,
-          height
-        });
-      });
-    }
-  }, []);
-
-  const handleSelectionChange = useCallback(
-    (box) => {
-      setSelectionBox(box);
-      const indexesToSelect = [];
-      selectableItems.current.forEach((item, index) => {
-        if (boxesIntersect(box, item)) {
-          indexesToSelect.push(index);
-        }
-      });
-
-      setSelectedIndexes(indexesToSelect);
-    },
-    [selectableItems]
-  );
-
-  return (
-    <div className="container">
-      <MouseSelection onSelectionChange={handleSelectionChange} />
-      <div id="elements-container" className="elements-container">
-        {Array.from({ length: 16 }, (_, i) => (
-          <div
-            key={i}
-            className={`element ${
-              selectedIndexes.includes(i) ? "selected" : ""
-            } `}
-          />
-        ))}
-      </div>
-
-      <div className="selection-box-info">
-        Selection Box:
-        <div>top: {selectionBox?.top || ""}</div>
-        <div>left: {selectionBox?.left || ""}</div>
-        <div>width: {selectionBox?.width || ""}</div>
-        <div>height: {selectionBox?.height || ""}</div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+<defs id="SHAPE34_defs"><linearGradient id="SHAPE34_desktop_gradient" gradientTransform="rotate(0)"><stop offset="0%" stop-color="#00dbde"></stop><stop offset="100%" stop-color="#fc00ff"></stop></linearGradient></defs>
+  
+  
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 129.62 192.74" class="" fill="url(&quot;#SHAPE34_desktop_gradient&quot;)"><defs id="SHAPE34_defs"><linearGradient id="SHAPE34_desktop_gradient" gradientTransform="rotate(0)"><stop offset="0%" stop-color="#00dbde"></stop><stop offset="100%" stop-color="#fc00ff"></stop></linearGradient></defs><path d="M129.6,9.14A9.13,9.13,0,0,0,120.71,0V0H0V.09A9.11,9.11,0,0,1,8.27,9.14v183.6L69,147.53l60.67,45.21V9.14Z"></path></svg>
+    
+    
+#SHAPE34 svg:last-child {
+    fill: url(#SHAPE34_desktop_gradient);
+}
