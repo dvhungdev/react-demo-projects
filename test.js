ec81070a8fd3b425d939ca42a0df4553fb9e8bff
dvhungdev <68392174+dvhungdev@users.noreply.github.com>
2021-07-19 13:39:43 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 <defs id="SHAPE34_defs"><linearGradient id="SHAPE34_desktop_gradient" gradientTransform="rotate(0)"><stop offset="0%" stop-color="#00dbde"></stop><stop offset="100%" stop-color="#fc00ff"></stop></linearGradient></defs>
   
+<defs id="SHAPE34_defs"><radialGradient id="SHAPE34_desktop_gradient" gradientTransform="rotate(0)"><stop offset="0%" stop-color="#00dbde"></stop><stop offset="100%" stop-color="#fc00ff"></stop></radialGradient></defs>  
   
   <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 129.62 192.74" class="" fill="url(&quot;#SHAPE34_desktop_gradient&quot;)"><defs id="SHAPE34_defs"><linearGradient id="SHAPE34_desktop_gradient" gradientTransform="rotate(0)"><stop offset="0%" stop-color="#00dbde"></stop><stop offset="100%" stop-color="#fc00ff"></stop></linearGradient></defs><path d="M129.6,9.14A9.13,9.13,0,0,0,120.71,0V0H0V.09A9.11,9.11,0,0,1,8.27,9.14v183.6L69,147.53l60.67,45.21V9.14Z"></path></svg>
     
