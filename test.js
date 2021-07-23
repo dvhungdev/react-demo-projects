aa4919899fe595af9f057ae8495818d2eddd6cb8
dvhungdev <68392174+dvhungdev@users.noreply.github.com>
2021-07-23 14:11:53 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,46 @@
-<!-- AMIS aiMarketing Form Embed Code -->
-                <div class="aimarketing-form-embed-code">
-                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-                    <script env=testonline id="ladizone-form-renderer" src="https://testcdnamisapp.misa.vn/apps/aimarketing/form-editor/static/js/form-renderer.js"></script>
-                    <script render-for="eb663302-4c3d-495d-ba78-97d2598f995b">
-                    lzfr.renderExternalForm({formId: "eb663302-4c3d-495d-ba78-97d2598f995b", tenantId: "3a73b72a-fe0c-44b2-8274-95f3bedd61b9", domainUnomi: "https://test.ladizone.com"});
-                    </script>
-                </div>
-             <!-- end AMIS aiMarketing Form Embed Code -->
+npm i react-draft-wysiwyg draft-js draftjs-to-html
 
+import { convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
- https://testamis.misa.vn/wp_login/
- test/12345678@Abc
+import "./App.css";
 
+function App() {
+  const [editorState, setEditorState] = useState("");
 
-https://testamis.misa.vn/17223/ve-chan-dung-khach-hang/?preview_id=17223&preview_nonce=42a81fe72f&post_format=standard&_thumbnail_id=-1&preview=true
+  const onEditorStateChange = (contentState) => {
+    console.log(contentState);
+    console.log("TEXT: ", draftToHtml(convertToRaw(contentState.getCurrentContent())));
+    setEditorState(contentState);
+  };
+
+  return (
+    <div className="App">
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        // onContentStateChange={onEditorStateChange}
+        onEditorStateChange={onEditorStateChange}
+        toolbar={{
+          options: [
+            "inline",
+            "fontSize",
+            "fontFamily",
+            "list",
+            "textAlign",
+            "colorPicker",
+            "link",
+            "embedded",
+          ],
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
